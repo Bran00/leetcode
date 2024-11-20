0e1ea1d458d4616dfbfb96281c76889718b4233f
Hildebrando Mendes <brandorocha00@gmail.com>
2024-11-20 19:04:58 -0300

    feat: add severalAnas

--- groupAnagram/script.js
+++ groupAnagram/script.js
@@ -1,26 +1,75 @@
 var groupAnagrams = function (strs) {
-  let newArray = [];
-  let loadArray = [];
+  let exists = []
+  let lias = []
 
-  for (let i = 0; i < strs.length; i++) {
-    loadArray = []
-    let firstAna = strs[i].split('').sort().join('')
-    loadArray.push(strs[i])
-    for(let t = i; t < strs.length; t++) {
-      let secondAna = strs[t].split('').sort().join('')
-      if(firstAna === secondAna) {
-        loadArray.push(strs[i])
-      }
-    }
-    newArray.push(loadArray)
-    if(newArray.length === strs.length){
-      console.log(newArray.length === strs.length)
-      break
+  for (let index = 0; index < strs.length; index++) {
+    const element = strs[index];
+    exists.push(element)
+
+
+    if (lias.find(e => e[0].split('').sort().join('') === element.split('').sort().join(''))) {
+      lias.forEach((e) => {
+        if (e[0].split('').sort().join('') === element.split('').sort().join('')) {
+          e.push(element)
+        }
+      })
+    } else {
+      lias.push([element])
     }
   }
-  console.log(newArray)
+
+  return lias
 };
 
-let strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+var groupAnagrams = function (strs) {
+  const map = new Map();
+
+  for (const str of strs) {
+    const key = str.split('').sort().join('');
+
+    if (!map.has(key)) {
+      map.set(key, []);
+    }
+    map.get(key).push(str);
+  }
+
+  return Array.from(map.values());
+};
+
+
+let strs = ["nozzle", "punjabi", "waterlogged", "imprison", "crux", "numismatists", "sultans", "rambles", "deprecating", "aware", "outfield", "marlborough", "guardrooms", "roast", "wattage"]
 
 console.log(groupAnagrams(strs))
+
+
+/*const LOWER_A = 97;
+const PRIME_NUMBERS = [
+  2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101,
+]; // 26 prime numbers
+
+console.log(PRIME_NUMBERS.length);
+
+const signature = (str) => {
+  let sig = 1;
+  for (let i = 0; i < str.length; i += 1) {
+    sig *= PRIME_NUMBERS[str.charCodeAt(i) - LOWER_A];
+  }
+  return sig;
+};
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+/*const groupAnagrams = (strs) => {
+  const map = new Map();
+  strs.forEach((str) => {
+    const sig = signature(str);
+    if (map.has(sig)) {
+      map.get(sig).push(str);
+    } else {
+      map.set(sig, [str]);
+    }
+  });
+  return [...map.values()];
+}; */
